bea632ef10088678a08af4f822b119f7a817677a
Zhi.Huang <zh.impact@outlook.com>
2026-04-30 21:52:12 +0800

    update: md2html support css and html in markdown toggle

--- packages/evaluate/src/evaluates/md2html.ts
+++ packages/evaluate/src/evaluates/md2html.ts
@@ -10,27 +10,43 @@ import { unified } from 'unified'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-async function main(input: string, output: string) {
+type Options = {
+  output: string
+  html?: boolean
+  style?: string
+}
+async function main(input: string, options: Options) {
   const processor = unified()
     .use(remarkParse)
     .use(remarkGfm)
-    .use(remarkRehype, { allowDangerousHtml: true })
-    .use(rehypeStringify)
+    .use(remarkRehype, { allowDangerousHtml: options.html })
+    .use(rehypeStringify, { allowDangerousHtml: options.html })
 
   const md = await readFile(input, 'utf-8')
 
-  const file = await processor.process(md)
+  const htmlContent = await processor.process(md)
 
-  await writeFile(output, String(file), 'utf-8')
+  let finalHtml = String(htmlContent)
+
+  if (options.style) {
+    const css = await readFile(options.style, 'utf-8')
+    const styleTag = `<style>${css}</style>`
+    const htmlWithStyle = `${styleTag}\n${String(htmlContent)}`
+    finalHtml = htmlWithStyle
+  }
+
+  await writeFile(options.output, finalHtml, 'utf-8')
 }
 
 export const md2html = new Command('md2html')
 
 md2html
   .option('-o, --output [path]', 'output path for the generated HTML file')
+  .option('--html', 'Support raw HTML in the Markdown input')
+  .option('-s, --style [path]', 'path to the CSS file to use for styling the HTML output')
   .argument('<input>', 'path to the input Markdown file')
   .action(async (input, options) => {
     const inputDir = dirname(input)
-    const output = options.output ?? join(inputDir, `${basename(input, extname(input))}.html`)
-    await main(input, output)
+    options.output = options.output ?? join(inputDir, `${basename(input, extname(input))}.html`)
+    await main(input, options)
   })
