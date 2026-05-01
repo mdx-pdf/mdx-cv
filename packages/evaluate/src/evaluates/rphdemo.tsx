0dacc86a1b776edf8894126ee54aef5024fa272d
Zhi.Huang <zh.impact@outlook.com>
2026-05-01 16:05:58 +0800

    update: cli flag support --reset-styles

--- packages/evaluate/src/evaluates/rphdemo.tsx
+++ packages/evaluate/src/evaluates/rphdemo.tsx
@@ -9,30 +9,32 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 interface Demo1Props {
   html: string
+  resetStyles: boolean
 }
-function Demo({ html }: Demo1Props) {
+function Demo({ html, resetStyles }: Demo1Props) {
   return (
     <Document>
       <Page>
-        <Html resetStyles={false}>{html}</Html>
+        <Html resetStyles={resetStyles}>{html}</Html>
       </Page>
     </Document>
   )
 }
 
-async function main(input: string, output: string) {
+async function main(input: string, output: string, resetStyles: boolean) {
   const html = await readFile(input, 'utf-8')
 
-  await ReactPDF.render(<Demo html={html} />, output)
+  await ReactPDF.render(<Demo html={html} resetStyles={resetStyles} />, output)
 }
 
 export const rphdemo = new Command('rphdemo')
 
 rphdemo
   .option('-o, --output [path]', 'output path for the generated PDF file')
+  .option('-r, --reset-styles', 'reset styles in Html component')
   .argument('<input>', 'path to the input HTML file')
   .action(async (input, options) => {
     const inputDir = dirname(input)
     const output = options.output ?? join(inputDir, `${basename(input, extname(input))}.pdf`)
-    await main(input, output)
+    await main(input, output, options.resetStyles)
   })
